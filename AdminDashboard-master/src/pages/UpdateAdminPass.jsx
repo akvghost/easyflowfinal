bc8d86b9a52dafa16ed6d851ed8af6c8ebf439dc
neha2619 <nehachaurasia2619@gmail.com>
2022-06-03 09:52:21 +0530

    page update pass created and modified

--- AdminDashboard-master/src/pages/UpdateAdminPass.jsx
+++ AdminDashboard-master/src/pages/UpdateAdminPass.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 
 export const UpdateAdminPass = () => {
   return (
-    <div>UpdateAdminPass</div>
-  )
-}
+    <div>
+      <h2 className="page-header">Admin</h2>
+      <hr className="featurette-divider" />
+      <h4 className="page-header">Update Password</h4>
+      <div className="container">
+        <form>
+          <div className="col-md-6">
+            <label htmlFor="inputEmail4" className="form-label">
+              Password
+            </label>
+            <input type="password" name="password" className="form-control" id="inputEmail4" />
+          </div>
+          <div className="col-6">
+            <label htmlFor="inputEmail4" className="form-label">
+              Confirm Password
+            </label>
+            <input type="password" name="confirmPassword" className="form-control" id="inputEmail4" />
+          </div>
+          <div className="col-12 my-4">
+
+            <button type="submit" className="btn btn-primary">
+              Update Password
+            </button>
+
+          </div>
+        </form>
+      </div>
+    </div>
+)}
--- easyflow-user-master/src/pages/UpdateWorkerPass.jsx
+++ easyflow-user-master/src/pages/UpdateWorkerPass.jsx
@@ -1,83 +1,19 @@
 import React, {useEffect} from 'react'
 import axios from 'axios'
-// add the path below where you want to update worker password
-const url = ""
+
 export const UpdateWorkerPass = (props) => {
-    props.state = {
-        password: '',
-        confirmPassword: ''
-      }
-    
-      const handleChange = (e) => {
-        const name = e.target.name
-        const value = e.target.value
-        this.setState({
-          [name]: value
-      })
-      }
-      const handleSubmit = (e) => {
-        e.preventDefault()
-        if(this.state.id){
-          updateRecord()
-        }else{
-          console.log("No Record found!")
-        }
-      }
-      const getCompanyId = () => {
-        const { id } = this.state
-            const comapnyId = url + id
-            console.log(comapnyId)
-            axios.get(comapnyId)
-                .then((res) => res.json())
-                .then((results) => {
-                    if(results){
-                        this.setState({
-                          password: results.password,
-                        });
-                    }else{
-                        alert("No Record Fount!")
-                    }
-                }
-                )
-      }
-      const updateRecord = () => {
-          if(this.state.password !== this.state.confirmPassword){
-              alert("password does'nt matches")
-          }
-        let head = {
-          password: this.state.password
-      }
-      const baseUrl = url + this.state.id
-      console.log(baseUrl)
-      axios.post(baseUrl, head)
-          .then((res) => {
-              return res.json()
-          })
-          .then((results) => {
-              if(results){
-                  alert("company Updated Successfully!")
-              }
-          })
-          .catch((error) => {
-              alert("Can not update company, there is an error")
-              console.log(error)
-          })
-      }
-      useEffect(() => {
-          getCompanyId()
-      })
-    return (
+        return (
         <div>
             <h2 className="page-header">Worker</h2>
             <hr className="featurette-divider" />
-            <h4 className="page-header">Update Profile</h4>
+            <h4 className="page-header">Update Password</h4>
             <div className="container">
-                <form className="row g-3" onSubmit={handleSubmit}>
+                <form>
                     <div className="col-md-6">
                         <label htmlFor="inputEmail4" className="form-label">
                             Password
                         </label>
-                        <input type="password" name="password" value={this.state.password} onChange={handleChange} className="form-control" id="inputEmail4" />
+                        <input type="password" name="password" className="form-control" id="inputEmail4" />
                     </div>
                     <div className="col-6">
                         <label htmlFor="inputEmail4" className="form-label">
@@ -85,7 +21,7 @@ export const UpdateWorkerPass = (props) => {
                         </label>
                         <input type="password" name="confirmPassword" className="form-control" id="inputEmail4" />
                     </div>
-                    <div className="col-12">
+                    <div className="col-12 my-4">
 
                         <button type="submit" className="btn btn-primary">
                             Update Password
